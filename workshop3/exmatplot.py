554c22c2ab6c567038a0e868f54a262a44ff50e3
Matthew Tsang <tsangma@umich.edu>
2020-02-11 23:55:58 -0500

    finished workshops

---
+++ workshop3/exmatplot.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Our data...
+x = np.linspace(0, 10, 100)
+y1, y2, y3 = np.cos(x), np.cos(x + 1), np.cos(x + 2)
+names = ['Signal 1', 'Signal 2', 'Signal 3']
+
+fig=plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+x = np.linspace(-5,5,100)
+ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position('center')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+plt.plot(x, y1, label = names[0])
+plt.plot(x, y2, label = names[1])
+plt.plot(x, y3, label = names[2])
+plt.legend(loc='upper left')
+
+plt.show()
